ae463bcce06f2fe527af4729e7dd632a9b1ee4a0
Iranoan <iranoan+vim@gmail.com>
2024-03-19 23:58:29 +0900

    use ssconvert.sh

--- bin/xlsx2table.sh
+++ bin/xlsx2table.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 # スプレッドシートを | で列の揃った形式で出力
-#
-# ssconvert.sh を呼び出し、その出力を処理すると、シートごとに列幅が異なると見にくくなる
 
-csv=$HOME/.tmp/$(basename $1).csv
-ssconvert --export-file-per-sheet "$1" "$csv" 1>/dev/null 2>&1
-
-for f in "$csv".*
-do
-	"$HOME/bin/csv2tsv.awk" "$f" | "$HOME/bin/tsv2table.awk"
-	echo ''
-	rm "$f"
-done
+ssconvert.sh "$1" |
+	"$HOME/bin/csv2tsv.awk" |
+	"$HOME/bin/tsv2table.awk"
