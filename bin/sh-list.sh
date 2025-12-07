5b8b7a3733dca75ea8db614fd133420c0724826a
Iranoan <iranoan+vim@gmail.com>
2025-12-08 00:00:40 +0900

    list up AWK/Perl/Ruby/sh

--- bin/sh-list.sh
+++ bin/sh-list.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-# ~/bin/, ~/bin にある *.sh のシェルスクリプトの一覧を作成
+# ~/bin/, ~/bin にある *.awk, *.pl, *.rb, *.sh のシェルスクリプトの一覧を作成
 # ファイル名とともに、そのファイルの先頭に有るコメントを表示
 # たいていファイルの先頭部分に簡単な説明を書いているので
 # ただし自分自身は除く
-# 思いつくシェルのシバンとデバッグ目的で書いてそうな記述削除
+# 思いつくスクリプトのシバンとデバッグ目的で書いてそうな記述削除
 
-find -L "$HOME/bin/" -maxdepth 1 -type f \( -name "*.sh" -a ! -name "${0##*/}" \) |
+find -L "$HOME/bin/" -maxdepth 1 -type f \( \( -name "*.awk" -o -name "*.pl" -o -name "*.rb" -o -name "*.sh" \) -a ! -name "${0##*/}" \) |
 	sort |
 	while read -r f ; do
 	printf '\033[1;7m%s\033[0;m\n' "${f##*/}"
 	awk '{
 		if ( /^$/ )exit
-		if ( /^#/ && !/^#!\/bin\/(ba|a|da|c|tc|z|k|fi)?sh/ && !/^\s*#?\s*set -[xe]/ ) {
+		if ( /^#/ && !/^#!\/(usr\/)?bin\/((ba|a|da|c|tc|z|k|fi)?sh|[gm]?awk|perl|env (perl|ruby|[mg]?awk))/ && !/^\s*#?\s*set -[xe]/ ) {
 			gsub("^#", "    ", $0 )
 			print $0
 		}
